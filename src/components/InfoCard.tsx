5f29b923bcb7e8b0d1af9f88137f0e384eb33227
cbf0055 <cbf0055@unt.edu>
2024-01-20 12:49:37 -0600

    Added bridal party pics

--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -5,6 +5,13 @@ import gavin from "../assets/gavin.jpg";
 import joe from "../assets/joe.jpg";
 import nick from "../assets/nick.jpg";
 import kyle from "../assets/kyle.jpg";
+import jeremy from "../assets/jeremy.jpg";
+import crimson from "../assets/crimson.jpg";
+import emily from "../assets/emily.jpg";
+import isabel from "../assets/isabel.jpg";
+import liv from "../assets/liv.jpg";
+import alyson from "../assets/alyson.jpg";
+import megan from "../assets/megan.jpg";
 
 const InfoCard = () => {
   return (
@@ -46,6 +53,62 @@ const InfoCard = () => {
         <CardBody>
           <Heading fontSize="2xl">Kyle Hunter</Heading>
           <p>Groomsmen</p>
+        </CardBody>
+      </Card>
+
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={jeremy}></Image>
+        <CardBody>
+          <Heading fontSize="2xl">Jeremy Kracy</Heading>
+          <p>Groomsmen</p>
+        </CardBody>
+      </Card>
+
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={crimson}></Image>
+        <CardBody>
+          <Heading fontSize="2xl">Crimson Scott</Heading>
+          <p>Maid of Honor</p>
+        </CardBody>
+      </Card>
+
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={isabel}></Image>
+        <CardBody>
+          <Heading fontSize="2xl">Isabel Vargas</Heading>
+          <p>Bridesmaid</p>
+        </CardBody>
+      </Card>
+
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={emily}></Image>
+        <CardBody>
+          <Heading fontSize="2xl">Emily Mask</Heading>
+          <p>Bridesmaid</p>
+        </CardBody>
+      </Card>
+
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={liv}></Image>
+        <CardBody>
+          <Heading fontSize="2xl">Olivia Garcia</Heading>
+          <p>Bridesmaid</p>
+        </CardBody>
+      </Card>
+
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={alyson}></Image>
+        <CardBody>
+          <Heading fontSize="2xl">Alyson Franklin</Heading>
+          <p>Bridesmaid</p>
+        </CardBody>
+      </Card>
+
+      <Card borderRadius={10} overflow="hidden">
+        <Image src={megan}></Image>
+        <CardBody>
+          <Heading fontSize="2xl">Meagan McKee</Heading>
+          <p>Bridesmaid</p>
         </CardBody>
       </Card>
     </>
