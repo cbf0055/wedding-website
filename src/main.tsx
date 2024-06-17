a7ccc140490c2ac7678106d9dda9d7a66390523f
cbf0055 <cbf0055@unt.edu>
2024-06-16 19:44:06 -0500

    commented out theme in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>
