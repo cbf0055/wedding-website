9cbd72852aa194ed2179a5a854e61eaf76f6e21a
cbf0055 <cbf0055@unt.edu>
2024-06-16 19:37:45 -0500

    removed color mode switch from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,6 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript
-        initialColorMode={theme.config.initialColorMode}
-      ></ColorModeScript>
       <App />
     </ChakraProvider>
   </React.StrictMode>
